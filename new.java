69af0f2129159c90107af1b48a4d1b1de48e03d9
srishtikapoor0240 <srishtikapoor0240@gmail.com>
2025-06-03 00:48:30 +0530

    Initial commit from terminal

---
+++ new.java
@@ -0,0 +1,6 @@
+public class new {
+    public static void main(String[] args) {
+        System.out.println("Srishti Kapoor ");
+        System.out.println("This is a new file created in the .git directory.");
+    }
+}
